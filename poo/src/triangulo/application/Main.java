73028fe8d932d48f0eaadd1126aec86c48e99dec
Ssousuke <wesleysousukee@gmail.com>
2021-11-18 02:02:51 -0300

    resolvendo problema com orientacao a objetos

--- poo/src/triangulo/application/Main.java
+++ poo/src/triangulo/application/Main.java
@@ -1,31 +1,37 @@
 package triangulo.application;
 
+import triangulo.entities.Triangulo;
+
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Triangulo {
+public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Locale.setDefault(Locale.US);
 
-        double xA, xB, xC, yA, yB, yC;
+        Triangulo x, y;
+
+        x = new Triangulo();
+        y = new Triangulo();
+
 
         System.out.println("Entre com as médidas do triangulo X: ");
-        xA = sc.nextDouble();
-        xB = sc.nextDouble();
-        xC = sc.nextDouble();
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
 
         System.out.println("Entre com as médidas do triangulo Y: ");
-        yA = sc.nextDouble();
-        yB = sc.nextDouble();
-        yC = sc.nextDouble();
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
 
-        double p = (xA + xB + xC) / 2;
+        double p = (x.a + x.b + x.c) / 2;
 
-        double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p - xC));
+        double areaX = Math.sqrt(p * (p - x.a) * (p - x.b) * (p - x.c));
 
-        p = (yA + yB + yC) / 2;
-        double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p - yC));
+        p = (y.a + y.b + y.c) / 2;
+        double areaY = Math.sqrt(p * (p - y.a) * (p - y.b) * (p - y.c));
 
         System.out.printf("Triangulo X: %.4f%n", areaX);
         System.out.printf("Triangulo Y: %.4f%n", areaY);
---
+++ poo/src/triangulo/entities/Triangulo.java
@@ -0,0 +1,7 @@
+package triangulo.entities;
+
+public class Triangulo {
+    public double a;
+    public double b;
+    public double c;
+}
