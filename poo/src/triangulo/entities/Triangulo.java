099092c471b799435bf4a34e91cf8cf7be3e7c15
Ssousuke <wesleysousukee@gmail.com>
2021-11-18 02:14:03 -0300

    adiciona método para cálculo de área

--- poo/src/triangulo/entities/Triangulo.java
+++ poo/src/triangulo/entities/Triangulo.java
@@ -4,4 +4,10 @@ public class Triangulo {
     public double a;
     public double b;
     public double c;
+
+    public double calculaAreaTriangulo() {
+        double p = (a + b + c) / 2;
+       double resultado = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+       return resultado;
+    }
 }
