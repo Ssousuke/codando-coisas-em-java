c12a7f893f027475b478956f75c79a2df5304f70
Ssousuke <wesleysousukee@gmail.com>
2022-05-19 01:28:40 -0300

    herança Alura

---
+++ herancaPolimorfismo/src/aluraHeranca/Gerente.java
@@ -0,0 +1,15 @@
+package aluraHeranca;
+
+public class Gerente extends Funcionario {
+    private int senha;
+
+    public boolean autentica(int senha) {
+        return this.senha == senha;
+    }
+
+    @Override
+    public double getBonificacao() {
+        // super indica que o atributo vem da super classe
+        return super.getBonificacao()+ super.getSalario();
+    }
+}
---
+++ herancaPolimorfismo/src/aluraHeranca/Main.java
@@ -0,0 +1,7 @@
+package aluraHeranca;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
